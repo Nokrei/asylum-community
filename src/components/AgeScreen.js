88ab56ac0dac9d8b36c2ee7fa5ef71a51efaeb81
Nok <dive877@gmail.com>
2021-03-10 22:50:56 +0400

    Add fade in and fade out animations from react spring to age screen

--- src/components/AgeScreen.js
+++ src/components/AgeScreen.js
@@ -1,7 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
+import { useSpring, animated, config } from "react-spring";
 import "./AgeScreen.scss";
 
 const AgeScreen = () => {
+  const [fade,setFade] = useState({
+    reset: false,
+    reverse:false
+  })
+  const fadeFromUp = useSpring({
+    from: {
+      marginTop: -200,
+      opacity: 0,
+    },
+    to: {
+      marginTop: 0,
+      opacity: 1,
+    },
+    config: {
+      duration: 200,
+      
+    },
+    reset:fade.reset,
+      reverse:fade.reverse
+  });
+  
+  const fadeDown = useSpring({
+    from: {
+      marginTop: 400,
+      opacity: 0,
+    },
+    to: {
+      marginTop: 0,
+      opacity: 1,
+    },
+    config: {
+      duration: 200, 
+    },
+    reset:fade.reset,
+      reverse:fade.reverse
+  });
+  
+  const handleConfirmClick = () =>{
+   setFade({
+     reset:true,
+     reverse:true
+   })
+  }
+
   return (
     <div className="age-screen">
       <video autoPlay muted loop className="background-vid">
@@ -12,12 +57,14 @@ const AgeScreen = () => {
       </video>
       <div className="top-text">Asylum 669</div>
       <div className="age-text">
-        <div className="age-text__question">
+        <animated.div style={fadeFromUp} className="age-text__question">
           ARE YOU OVER <br />
           18 YEARS OLD
-        </div>
-        <div className="age-text__answer">
-          <div className="age-text__answer__item age-text__answer__confirm">
+        </animated.div>
+     
+       
+        <animated.div style={fadeDown} className="age-text__answer">
+          <div className="age-text__answer__item age-text__answer__confirm" onClick={handleConfirmClick}>
             IM 18+
             <span class="Mask">
               <span>IM 18+</span>
@@ -26,7 +73,7 @@ const AgeScreen = () => {
               <span>IM 18+</span>
             </span>
           </div>
-          <div className="age-text__answer__item age-text__answer__deny">
+          <div className="age-text__answer__item age-text__answer__deny" onClick={handleConfirmClick}>
             NO
             <span class="Mask">
               <span>NO</span>
@@ -35,7 +82,7 @@ const AgeScreen = () => {
               <span>NO</span>
             </span>
           </div>
-        </div>
+        </animated.div>
       </div>
     </div>
   );
