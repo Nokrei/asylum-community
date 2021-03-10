60ccceb41694a8cf8bdd0ed53f87817b4238d17d
Nok <dive877@gmail.com>
2021-03-10 15:44:58 +0400

    Age page prototype

---
+++ src/components/AgeScreen.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./AgeScreen.scss";
+import backgroundVid from "../resources/background.mov";
+const AgeScreen = () => {
+  return (
+    <div className="age-screen">
+      <video autoPlay muted loop className="background-vid">
+        <source
+          src="https://cdn.videvo.net/videvo_files/video/free/2013-09/small_watermarked/OMSBG5_preview.webm"
+          type="video/mp4"
+        />
+      </video>
+      <div className="top-text"></div>
+      <div className="age-text">
+        <div className="age-text__question">
+          ARE YOU OVER <br />
+          18 YEARS OLD
+        </div>
+        <div className="age-text__anwser">
+          <div className="age-text__anwser__item age-text__anwser__confirm">
+            IM 18+
+            <span class="Mask">
+              <span>IM 18+</span>
+            </span>
+            <span class="Mask">
+              <span>IM 18+</span>
+            </span>
+          </div>
+          <div className="age-text__anwser__item age-text__anwser__deny">
+            NO
+            <span class="Mask">
+              <span>NO</span>
+            </span>
+            <span class="Mask">
+              <span>NO</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AgeScreen;
---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AgeScreen from './AgeScreen'
+
+function App() {
+  return (
+    <div className="App">
+      <AgeScreen/>
+    </div>
+  );
+}
+
+export default App;
