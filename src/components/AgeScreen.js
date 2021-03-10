afb3a89656663ab5084ccd980ee37330cbc29b70
Nok <dive877@gmail.com>
2021-03-10 16:34:42 +0400

    Add media queries for mobile, fix typo in class name

--- src/components/AgeScreen.js
+++ src/components/AgeScreen.js
@@ -10,14 +10,14 @@ const AgeScreen = () => {
           type="video/mp4"
         />
       </video>
-      <div className="top-text"></div>
+      <div className="top-text">Asylum 669</div>
       <div className="age-text">
         <div className="age-text__question">
           ARE YOU OVER <br />
           18 YEARS OLD
         </div>
-        <div className="age-text__anwser">
-          <div className="age-text__anwser__item age-text__anwser__confirm">
+        <div className="age-text__answer">
+          <div className="age-text__answer__item age-text__answer__confirm">
             IM 18+
             <span class="Mask">
               <span>IM 18+</span>
@@ -26,7 +26,7 @@ const AgeScreen = () => {
               <span>IM 18+</span>
             </span>
           </div>
-          <div className="age-text__anwser__item age-text__anwser__deny">
+          <div className="age-text__answer__item age-text__answer__deny">
             NO
             <span class="Mask">
               <span>NO</span>
