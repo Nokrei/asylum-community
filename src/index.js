00a6639b7faa82ccd6633c96a38772d2fbb9ef22
Nok <dive877@gmail.com>
2021-04-21 15:59:11 +0400

    Remove react-snap

--- src/index.js
+++ src/index.js
@@ -3,10 +3,4 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-const rootElement = document.getElementById("root");
-
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(<App />, rootElement);
-} else {
-  ReactDOM.render(<App />, rootElement);
-}
+ReactDOM.render(<App />, document.getElementById("root"));
