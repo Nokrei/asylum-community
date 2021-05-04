c77e159d2d962b816743ac161f22306c5bfc2cd6
Nok <dive877@gmail.com>
2021-05-04 21:55:13 +0400

    Change from background img to img, reposition mascot

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,17 @@ import "./App.scss";
 import "./Home.scss";
 const Home = (props) => {
   return (
-    <div style={{backgroundImage: `url(${mascot})`}} id="home" className="home-text__container">
+    <div  id="home" className="home-text__container">
       <div style={props.style} className="creepy-font home-text__big">
         <Link className="join-link" to="/main#join">
           <span >Jo</span>i
           <span >n</span>
         </Link>{" "}
         The <br /> Madness
+        
+      </div>
+      <div className='background-img-container'>
+      <img src={mascot} className='mascot'/>
       </div>
     </div>
   );
