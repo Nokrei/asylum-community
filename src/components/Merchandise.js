805a737d86be5e8570ebf552000f0f1a8300eb2e
Nok <dive877@gmail.com>
2022-01-19 23:20:23 +0400

    Add red bubble image and link to merchandise screen

--- src/components/Merchandise.js
+++ src/components/Merchandise.js
@@ -1,13 +1,39 @@
 import React from "react";
 import Footer from "./Footer";
+import redBubble from "../images/red-bubble.png";
 import "./App.scss";
 import "./Merchandise.scss";
+
 const Merchandise = () => {
   return (
     <div className="merchandise">
-      <div className="merchandise-text__big creepy-font">
-        Comming <br /> soon
-      </div>{" "}
+      <div className="red-bubble-container-outer">
+        <div className="red-bubble-container">
+          <p class="white-font">
+            Check out our merchandise at
+            <a
+              href="https://www.redbubble.com/shop/ap/97972062"
+              rel="noreferrer noopener"
+              target="_blank"
+              className="accent-font"
+            >
+              Red Bubble!
+            </a>{" "}
+          </p>
+          <a
+            href="https://www.redbubble.com/shop/ap/97972062"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            <img
+              className="red-bubble-image"
+              src={redBubble}
+              alt="Asylum 669 red bubble"
+            />
+          </a>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
